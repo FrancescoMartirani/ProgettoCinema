9336f6361953c58a351faa3908adfda5e5d33dad
FrancescoMartirani <104638397+FrancescoMartirani@users.noreply.github.com>
2022-05-27 11:43:15 +0200

    Add files via upload

---
+++ Manager/CinemaManager.cs
@@ -0,0 +1,39 @@
+﻿using ProgettoCinema.Models;
+using System.Data.SqlClient;
+
+namespace ProgettoCinema.Manager
+{
+    public class CinemaManager
+    {
+
+        private static string ConnectionString = @"Server = ACADEMYNETPD01\SQLEXPRESS; Database = Cinema; Trusted_Connection = True; ";
+
+        public List<Cinema> getAllCinemas()
+        {
+
+            List<Cinema> cinemasList = new List<Cinema>();
+            string sql = @"SELECT * FROM [dbo].[Cinemas]";
+
+            using var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var cinema = new Cinema
+                {
+                    idCinema = Convert.ToInt32(reader["IdCinema"].ToString()),
+                    nomeCinema = reader["NomeCinema"].ToString(),
+                    indirizzo = reader["Indirizzo"].ToString(),
+
+                };
+                cinemasList.Add(cinema);
+
+            }
+
+            return cinemasList;
+
+        }
+
+    }
+}
---
+++ Models/Cinema.cs
@@ -0,0 +1,14 @@
+﻿namespace ProgettoCinema.Models
+{
+    public class Cinema
+    {
+
+        public int idCinema { get; set; }
+
+        public string nomeCinema { get; set; }
+
+        public string indirizzo { get; set; }
+
+
+    }
+}
